d08e85c758ca72612f0a478af2e9ee06d1dbfd96
User <user@Users-MacBook-Pro-2.local>
2018-01-15 21:37:44 -0500

    queue length and fake queue length after task complete

--- src/main/java/edu/berkeley/sparrow/daemon/nodemonitor/NodeMonitor.java
+++ src/main/java/edu/berkeley/sparrow/daemon/nodemonitor/NodeMonitor.java
@@ -123,15 +123,12 @@ public class NodeMonitor {
     if (appId.equals("*")) {
       for (String app : appSockets.keySet()) {
         out.put(app, scheduler.getResourceUsage(app));
-        LOG.debug("QueueLength is " + (scheduler.getResourceUsage(app).queueLength));
-        LOG.debug("FakeQueueLength is " + (scheduler.getResourceUsage(app).fakeQueueLength));
+
 
       }
     }
     else {
       out.put(appId, scheduler.getResourceUsage(appId));
-      LOG.debug("QueueLength is " + (scheduler.getResourceUsage(appId).queueLength));
-      LOG.debug("FakeQueueLength is " + (scheduler.getResourceUsage(appId).fakeQueueLength));
     }
     LOG.debug("Returning " + out);
     return out;
@@ -143,6 +140,8 @@ public class NodeMonitor {
   public void tasksFinished(List<TFullTaskId> tasks) {
     LOG.debug(Logging.functionCall(tasks));
     scheduler.tasksFinished(tasks);
+    LOG.debug("QueueLength is " + (scheduler.getResourceUsage(tasks.get(0).appId).queueLength));
+    LOG.debug("FakeQueueLength is " + (scheduler.getResourceUsage(tasks.get(0).appId).fakeQueueLength));
   }
 
   /**
