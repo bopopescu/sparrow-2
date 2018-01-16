b2ccac806f033354657c5c6c9324d01c82416343
User <user@Users-MacBook-Pro-2.local>
2018-01-15 21:41:45 -0500

    fetch queue length after task is completed

--- src/main/java/edu/berkeley/sparrow/daemon/nodemonitor/NodeMonitor.java
+++ src/main/java/edu/berkeley/sparrow/daemon/nodemonitor/NodeMonitor.java
@@ -123,12 +123,10 @@ public class NodeMonitor {
     if (appId.equals("*")) {
       for (String app : appSockets.keySet()) {
         out.put(app, scheduler.getResourceUsage(app));
-        LOG.debug("QueueLength is " + (scheduler.getResourceUsage(app).queueLength));
       }
     }
     else {
       out.put(appId, scheduler.getResourceUsage(appId));
-      LOG.debug("QueueLength is " + (scheduler.getResourceUsage(appId).queueLength));
     }
     LOG.debug("Returning " + out);
     return out;
@@ -140,6 +138,7 @@ public class NodeMonitor {
   public void tasksFinished(List<TFullTaskId> tasks) {
     LOG.debug(Logging.functionCall(tasks));
     scheduler.tasksFinished(tasks);
+    LOG.debug("QueueLength is " + (scheduler.getResourceUsage(tasks.get(0).getAppId()).getQueueLength()));
   }
 
   /**
